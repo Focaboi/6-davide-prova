501be836d4dc157608ddcc5164d05e137f63e50f
fra-inteso <france060902@gmail.com>
2026-05-08 11:46:40 +0200

    fine classe metodi statici

--- src/spesa/settimanale/ArticoloSpesa.java
+++ src/spesa/settimanale/ArticoloSpesa.java
@@ -1,5 +1,49 @@
 package spesa.settimanale;
 
 public class ArticoloSpesa {
-
+	
+	
+	String nome;
+	String categoria;
+	double prezzoUnitario;
+	int quantita;
+	boolean acquistato;
+	
+	
+	
+	public double calcolaTotaleArticolo()
+	{
+		
+	}
+	
+	
+	
+	//per controllare se il prezzo è valido o no
+	public static boolean prezzoValido(double prezzo)
+	{
+		return prezzo >= 0;
+	}
+	
+	//per controllare se una quantità è valida
+	public static boolean quantitaValida(int quantita)
+	{
+		return quantita > 0;
+	}
+	
+	//confrontare due articoli in base al totale
+	public static ArticoloSpesa articoloConTotaleMaggiore(ArticoloSpesa a1, ArticoloSpesa a2)
+	{
+		if (a1.calcolaTotaleArticolo() >= a2.calcolaTotaleArticolo())
+		{
+			return a1;
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+	
 }
--- src/spesa/settimanale/GestioneListaSpesa.java
+++ src/spesa/settimanale/GestioneListaSpesa.java
@@ -3,8 +3,13 @@ package spesa.settimanale;
 public class GestioneListaSpesa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
